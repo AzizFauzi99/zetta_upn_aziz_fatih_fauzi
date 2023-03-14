c27a5e13087e08cbc7de534a790c19da3fcca4aa
Aziz Fatih Fauzi <92005833+AzizFauzi99@users.noreply.github.com>
2023-03-14 20:11:27 +0700

    js day 3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function purchaseBook(title, author, price, discountPercentage, stockAmount, purchaseAmount) {
+function purchaseBook(title, author, price, discountPercentage, stockAmount, purchaseAmount, creditTerm) {
 
     // Define constant tax percentage = 11%
     const taxPercentage = 11;
@@ -15,6 +15,8 @@ function purchaseBook(title, author, price, discountPercentage, stockAmount, pur
     // Inisialisasi variabel
     let totalPurchaseAmount = 0;
     let totalPrice = 0;
+    let installmentPrice = 0;
+    let installmentTerm = [];
 
     // Looping selama stock masih ada dan purchase amount lebih kecil dari stock
     for (let i = 0; i < purchaseAmount && stockAmount > 0; i++) {
@@ -33,8 +35,20 @@ function purchaseBook(title, author, price, discountPercentage, stockAmount, pur
 
       // Update jumlah stock 
       stockAmount--;
+
+      // Hitung installment (cicilan)
+      installmentPrice = totalPrice / creditTerm;
+      
     }
   
+    // Buat array untuk installment term objects untuk menyimpan tagihan yang harus dibayar tiap bulan
+    for (let j = 1; j <= creditTerm; j++) {
+      installmentTerm.push({
+        "Term": j,
+        "Price": installmentPrice.toFixed(2)
+      });
+    }
+
     // Display semua data
     console.log("Title: " + title);
     console.log("Author: " + author);
@@ -43,8 +57,9 @@ function purchaseBook(title, author, price, discountPercentage, stockAmount, pur
     console.log("Total purchase amount: " + totalPurchaseAmount);
     console.log("Total price: $" + totalPrice.toFixed(2));
     console.log("Stock amount: " + stockAmount);
+    console.log(installmentTerm);
   
     return totalPrice;
   }
 
-  purchaseBook("Koala Kumal", "Raditya Dika", 13.49, 15, 10, 6);
+  purchaseBook("Koala Kumal", "Raditya Dika", 13.49, 15, 10, 6, 6);
